4a8ea6e447281598f33330c3fe6d4ebaca2ea8da
Z3nitt <helios1612@gmail.com>
2024-03-14 14:10:55 +0100

    Personas

---
+++ Personas/src/model/Model/Persona.java
@@ -0,0 +1,68 @@
+package model.Model;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellido1;
+
+    /**
+     * 
+     */
+    private String apellido2;
+
+    /**
+     * 
+     */
+    private int edad = 0;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellido1 
+     * @param apellido2 
+     * @param edad
+     */
+    public void Persona(String dni, String nombre, String apellido1, String apellido2, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni
+     */
+    public void SetDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @return
+     */
+    public void getDni() {
+        // TODO implement here
+        return;
+    }
+
+}
